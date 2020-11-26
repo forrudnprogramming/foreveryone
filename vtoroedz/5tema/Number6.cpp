453df1a693e486defffdeb438b1b6cb12f031734
forrudnprogramming <75085468+forrudnprogramming@users.noreply.github.com>
2020-11-26 19:04:08 +0300

    Create Number6.cpp

---
+++ vtoroedz/5tema/Number6.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+int main() { 
+  int n; 
+  printf("Введите число: "); 
+  scanf("%d",&n); 
+  switch (n){ 
+  case 1: printf("I\n"); break; 
+  case 2: printf("II\n"); break; 
+  case 3: printf("III\n"); break; 
+  case 4: printf("IV\n"); break; 
+  case 5: printf("V\n"); break; 
+  case 6: printf("VI\n"); break; 
+  case 7: printf("VII\n"); break; 
+  case 8: printf("VIII\n"); break; 
+  case 9: printf("IX\n"); break; 
+  case 10: printf("X\n"); break; 
+  default: printf("Неверный номер\n"); 
+} 
+  return 0; 
+}
